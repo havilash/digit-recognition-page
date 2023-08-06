8befd3b8aed5ac5b59712664ca68a79587c8e15c
Havilash Sivaratnam <havilash2005@gmail.com>
2023-08-06 20:32:53 +0200

    update

--- website/digit_recognition_ai/model.py
+++ website/digit_recognition_ai/model.py
@@ -1,64 +1,92 @@
-import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import random
+from neural_network import activations, costs, nn as neural_network, layers
+from neural_network.data import get_augmented_mnist_data, train_test_split, get_mnist_data
+from neural_network.filters import ALL_FILTERS
+from PIL import Image, ImageOps
 
-"""data = tf.keras.datasets.mnist
+NN_PATH = "neural_network.pkl"
 
-(x_train, y_train), (x_test, y_test) = data.load_data()
-x_train = x_train / 255
-x_test = x_test / 255
+def recognize(image: Image):
+    with open(NN_PATH, 'rb') as f:
+        nn = neural_network.NeuralNetwork.load(f)
 
-
-def train_model():
-    model = tf.keras.Sequential(
-        [
-            tf.keras.layers.Flatten(input_shape=[28, 28]),
-            tf.keras.layers.Dense(128, activation="relu"),
-            tf.keras.layers.Dense(10, activation="softmax"),
-        ]
-    )
-
-    model.compile(
-        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
-    )
-    model.fit(x_train, y_train, epochs=10)
-
-    loss, acc = model.evaluate(x_test, y_test)
-    print("Accuracy:", acc)
-
-    model.save("model.h5")
-
-
-def recognize_test_data():
-    model = tf.keras.models.load_model("model.h5")
-
-    prediction = model.predict(x_test)
-    print(prediction)
-
-    plt.rcParams.update({"font.size": 6})
-    plt.figure(figsize=(9, 9))
-    for i in range(0, 8 * 2, 2):
-        p = random.randint(0, len(y_test))
-        plt.subplot(4, 4, i + 1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.grid(False)
-        plt.imshow(x_test[p], cmap=plt.cm.binary)
-        plt.xlabel(np.argmax(prediction[p]))
-
-        plt.subplot(4, 4, i + 1 + 1)
-        plt.bar(
-            range(0, 10),
-            prediction[p],
-            color=["black" if np.argmax(prediction[p]) == y_test[p] else "red"],
-        )
-
-    plt.show()"""
-
-
-def recognize(img):
-    model = tf.keras.models.load_model("model.h5")
-    prediction = model.predict(img)
+    img = image.convert('LA')
+    # img = ImageOps.invert(img)
+    img = img.resize((28, 28), Image.LANCZOS)
+    img = np.array(img)
+    img = [[p[1] for p in row] for row in img]
+    # img = image.convert('L')
+    # img = img.resize((28, 28), Image.LANCZOS)   
+    img = np.array(img)  / 255
+    img = img.reshape(28, 28)
+    print(img)
+    prediction = nn.predict(img)
+    
     return prediction
+    
+
+def train():    
+    nn = neural_network.NeuralNetwork([
+        layers.Conv2D(ALL_FILTERS),
+        # layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(26*26 * len(ALL_FILTERS), 128, activations.ReLU),
+        layers.Dense(128, 10, activations.Softmax),
+    ])
+
+    one_hot = lambda y: np.eye(10)[y]
+
+    x, y = get_mnist_data()
+    # x, y = get_augmented_mnist_data(10)  # needs some time
+    y = np.array([one_hot(i) for i in y])
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+    train_data = np.array(list(zip(x_train, y_train)), dtype=object)    
+    test_data = np.array(list(zip(x_test, y_test)), dtype=object)
+
+    train_accuracies, train_costs = nn.train(
+        train_data, 
+        test_data, 
+        learn_rate=0.2, 
+        cost=costs.CategoricalCrossEntropy, 
+        batch_size=64,
+        epochs=5,
+        save=True, 
+        file_name=NN_PATH,
+        validate_per_batch=False,
+        validate_interval=1,
+        learn_method="threading",
+    )
+    
+    #  plot train accuracies, costs
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(train_accuracies)
+    ax1.set_title('Accuracy')
+    ax1.set_ylim(0, 1)
+    ax2.plot(train_costs)
+    ax2.set_title('Cost')
+
+    with open(NN_PATH, 'rb') as f:
+        nn = neural_network.NeuralNetwork.load(f)
+
+
+    fig, axes = plt.subplots(3, 3, figsize=(6, 6))
+    for i in range(3):
+        for j in range(3):
+            k = np.random.randint(len(test_data))
+            axes[i, j].imshow(test_data[k][0].reshape((28, 28)), cmap='gray')
+            expected = np.argmax(test_data[k][1])
+            predicted = np.argmax(nn.predict(test_data[k][0]))
+            axes[i, j].set_title(f"Expected: {expected}\nPredicted: {predicted}")
+            axes[i, j].set_xticklabels([])
+            axes[i, j].set_yticklabels([])
+            axes[i, j].tick_params(axis='both', which='both', length=0)
+    fig.subplots_adjust(hspace=0.5)
+    plt.show()
+
+def main():
+    train()
+
+if __name__ == "__main__":
+    main()
--- website/views.py
+++ website/views.py
@@ -20,10 +20,7 @@ def upload_image():
     data = request.get_json()
     image_string = data["image"]
     image_data = base64.b64decode(image_string.split(",")[1])
-    image = Image.open(io.BytesIO(image_data)).convert("LA")
-    img_data = list(image.resize((28, 28)).getdata())
-    img_data = np.array([p[1] / 255 for p in img_data])
-    img_data = img_data.reshape((1, 28, 28))
-    result = recognize(img_data)
+    image = Image.open(io.BytesIO(image_data))
+    result = recognize(image)
 
-    return json.dumps({"message": "message received", "prediction": result.tolist()[0]})
+    return json.dumps({"message": "message received", "prediction": result.tolist()})
